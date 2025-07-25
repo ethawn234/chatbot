b0cb43a43cc4484d4dbd3f705580e00f55adb1d6
ethawn234 <ethawn234@gmail.com>
2025-07-25 11:16:34 -0500

    add embeddings & DB logic & configure dotenv

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,14 @@
+import os
+
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.environ.get("OPENAI_API_KEY")
 
 # Upload PDF files
 st.header('My Chatbot')
@@ -22,9 +30,19 @@ if file is not None:
   text_splitter = RecursiveCharacterTextSplitter(
     separators='\n',
     chunk_size=1000,
-    chunk_overlap=150, # defines overlap between chunks; eg chunk B may lose context as B may start mid-sentence, and context is contained in a sentence in A.
+    chunk_overlap=150,  # defines overlap between chunks; eg chunk B may lose context as B may start mid-sentence, and context is contained in a sentence in A.
     length_function=len
   )
 
-  chunks = text_splitter.split_text(text)
-  st.write(chunks)
+  chunk = text_splitter.split_text(text)
+  # st.write(chunks)
+
+  # generate embeddings
+  embeddings = OpenAIEmbeddings(api_key=api_key)  # configure api key usage via .env
+
+  # create vector store (FAISS: Facebook Semantic Search)
+  # FAISS is doing 3 things here:
+  # 1. generates embeddings
+  # 2. initializes FAISS
+  # 3. stores the chunks & embeddings
+  vector_store = FAISS.from_texts(chunk, embeddings)
