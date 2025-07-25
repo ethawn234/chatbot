085bb162226edcd8dc05b7ef8fcede20431b6c98
ethawn234 <ethawn234@gmail.com>
2025-07-25 08:46:25 -0500

    add logic to read pdf

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,20 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
 # Upload PDF files
-st.header("My Chatbot")
+st.header('My Chatbot')
 
 with st.sidebar:
-  st.title("Your documents")
-  file = st.file_uploader(" Upload a PDF file and start asking questions", type="pdf")
+  st.title('Your documents')
+  file = st.file_uploader(' Upload a PDF file and start asking questions', type='pdf')
+
+# Read PDF file
+if file is not None:
+  pdf_reader = PdfReader(file)
+  text = ''
+  for page in pdf_reader.pages:
+    text += page.extract_text()
+    # st.write(text)
+
+# Chunk the text
+
